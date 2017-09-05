c28e3853b3e1cf6f8a84e578bbc935bbc5bd9386
Spcie <m15188159686@163.com>
2017-09-05 17:02:03 +0800

    Update hw_gpio.h

--- hw_gpio.h
+++ hw_gpio.h
@@ -3,7 +3,8 @@
 
 #define PIN					26 //GPIO26 
 #define BCM2835_GPIO_ADDRESS_START           0x3f200000
-#define BCM2835_GPIO_ADDRESS_END             0x3f2000b3  
+#define BCM2835_GPIO_ADDRESS_END             0x3f2000b3
+#define BCM2835_GPIO_ADDRESS_LEN             (BCM2835_GPIO_ADDRESS_END - BCM2835_GPIO_ADDRESS_START)
 #define BCM2835_GPSET0                       0x001c  
 #define BCM2835_GPFSEL0                      0x0000  
 #define BCM2835_GPCLR0                       0x0028  
