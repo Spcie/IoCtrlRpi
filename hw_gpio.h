2d6586890709ae6c122a6916a4f644bcb0f1f6db
Spcie <m15188159686@163.com>
2017-09-05 16:36:01 +0800

    Update hw_gpio.h

--- hw_gpio.h
+++ hw_gpio.h
@@ -1,13 +1,13 @@
 #ifndef __HW_GPIO_H
 #define __HW_GPIO_H
 
-/*暂定*/
-#define BCM2835_GPIO_BASE			0x20200000
-#define BCM2835_GPIOReg_GPFSEL0		0x00000000
-#define BCM2835_GPIOReg_GPSET0		0x0000001c
-#define BCM2835_GPIOReg_GPCLR0		0x00000028
-#define BCM2835_GPIO_FSEL_INP		0x00000000
-#define BCM2835_GPIO_FSEL_OUTP		0x00000001
-#define RPI_BPLUS_GPIO_J8_12		18
+#define PIN					26 //GPIO26 
+#define BCM2835_GPIO_BASE           0x3f200000  
+#define BCM2835_GPSET0              0x001c  
+#define BCM2835_GPFSEL0             0x0000  
+#define BCM2835_GPCLR0              0x0028  
+#define BCM2835_GPIO_FSEL_OUTP      1  
+  
+
 
 #endif /*__HW_GPIO_H*/
