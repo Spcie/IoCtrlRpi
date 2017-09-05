9262f82fb7bb66c6f0fc4f89c73192c50e5585c7
Spcie <m15188159686@163.com>
2017-09-05 17:02:41 +0800

    Update hw_gpio.c

--- hw_gpio.c
+++ hw_gpio.c
@@ -1,7 +1,7 @@
 
 int bcm2835_gpio_fsel(uint8_t pin, uint8_t mode)
 {
-    volatile uint32_t * bcm2835_gpio = (volatile uint32_t *)ioremap(BCM2835_GPIO_BASE, 16);    
+    volatile uint32_t * bcm2835_gpio = (volatile uint32_t *)ioremap(BCM2835_GPIO_ADDRESS_START, BCM2835_GPIO_ADDRESS_LEN);    
     volatile uint32_t * bcm2835_gpio_fsel = bcm2835_gpio + BCM2835_GPFSEL0/4 + (pin/10);    
     uint8_t   shift = (pin % 10) * 3;    
     uint32_t  value = mode << shift;    
@@ -12,7 +12,7 @@ int bcm2835_gpio_fsel(uint8_t pin, uint8_t mode)
 
 int bcm2835_gpio_set(uint8_t pin)    
 {       
-    volatile uint32_t * bcm2835_gpio = (volatile uint32_t *)ioremap(BCM2835_GPIO_BASE, 16);    
+    volatile uint32_t * bcm2835_gpio = (volatile uint32_t *)ioremap(BCM2835_GPIO_ADDRESS_START, BCM2835_GPIO_ADDRESS_LEN);    
     volatile uint32_t * bcm2835_gpio_set = bcm2835_gpio + BCM2835_GPSET0/4 + pin/32;    
     uint8_t   shift = pin % 32;    
     uint32_t  value = 1 << shift;    
@@ -25,7 +25,7 @@ int bcm2835_gpio_set(uint8_t pin)
 
 int bcm2835_gpio_clr(uint8_t pin)    
 {      
-    volatile uint32_t * bcm2835_gpio = (volatile uint32_t *)ioremap(BCM2835_GPIO_BASE, 16);    
+    volatile uint32_t * bcm2835_gpio = (volatile uint32_t *)ioremap(BCM2835_GPIO_ADDRESS_START, BCM2835_GPIO_ADDRESS_LEN);    
     volatile uint32_t * bcm2835_gpio_clr = bcm2835_gpio + BCM2835_GPCLR0/4 + pin/32;    
     uint8_t   shift = pin % 32;    
     uint32_t  value = 1 << shift;    
