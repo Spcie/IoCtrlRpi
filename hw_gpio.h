e1e13cc41a8b95e3439621509937a00df350200b
Spcie <m15188159686@163.com>
2017-09-05 17:14:59 +0800

    Update hw_gpio.h

--- hw_gpio.h
+++ hw_gpio.h
@@ -8,8 +8,10 @@
 #define BCM2835_GPSET0                       0x001c  
 #define BCM2835_GPFSEL0                      0x0000  
 #define BCM2835_GPCLR0                       0x0028  
-#define BCM2835_GPIO_FSEL_OUTP               1  
-  
+#define BCM2835_GPIO_FSEL_OUTP               0x01  
+#define BCM2835_GPIO_FSEL_INP                0x00  
 
-
+int bcm2835_gpio_fsel(uint8_t pin, uint8_t mode);
+int bcm2835_gpio_set(uint8_t pin);
+int bcm2835_gpio_clr(uint8_t pin);
 #endif /*__HW_GPIO_H*/
