f95a2fd8db74c3e5fbd631b0bf965a997574091c
Spcie <m15188159686@163.com>
2017-09-11 23:37:28 +0800

    update

--- IoCtrlRpi/hw_gpio.c
+++ IoCtrlRpi/hw_gpio.c
@@ -1,17 +1,3 @@
-#include <linux/module.h>
-#include <linux/types.h>
-#include <linux/fs.h>
-#include <linux/errno.h>
-#include <linux/slab.h>
-#include <linux/mm.h>
-#include <linux/sched.h>
-#include <linux/init.h>
-#include <linux/cdev.h>
-#include <linux/slab.h>
-#include <asm/io.h>
-#include <asm/system.h>
-#include <asm/uaccess.h>
-
 #include "hw_gpio.h"
 
 uint32_t * bcm2835_gpio;
@@ -46,6 +32,3 @@ int bcm2835_gpio_clr(uint8_t pin)
 
 	return 0;
 }
-
-MODULE_AUTHOR("Spice");
-MODULE_LICENSE("GPL");
