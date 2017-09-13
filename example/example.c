db4b9b55d46e27050667327193c56fcbc52e8ed4
spcie <m15188159686@163.com>
2017-09-13 23:08:24 +0800

    update

--- example/example.c
+++ example/example.c
@@ -10,7 +10,7 @@ int main(void)
 	FILE *fb0 = NULL;
 	char Buf[256];
 
-	fb0 = fopen("/dev/IoCtrl", "r+");
+	fb0 = fopen("/dev/msic_IoCtrl", "r+");
 
 	if(fb0 == NULL)
 	{
@@ -18,7 +18,9 @@ int main(void)
 		return -1;
 	}
 
+	printf("write!\n");
 	fwrite(Buf,sizeof(Buf),1,fb0);
-
+	fclose(fb0);
+	printf("close IoCtrl!\n");
 	return 0;
 }
